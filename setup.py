57d66a60d5d3427726a4a75e159e5a06171a1696
zachasme <zachasme@gmail.com>
2015-05-11 14:42:52 +0200

    Use correct pypi package for statbank

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@ setup(name='statbanksync',
       description='Synchronize DST tables to local db',
       url='http://github.com/gisgroup/statbanksync',
       author='Gis Group ApS',
-      author_email='oliver@gisgroup.dk, valentin@gisgroup.dk, zacharias@gisgroup.dk',
+      author_email='oliver@gisgroup.dk, \
+                    valentin@gisgroup.dk, \
+                    zacharias@gisgroup.dk',
       license='MIT',
       packages=['statbanksync'],
       install_requires=[
-          'statbank',
+          'gisgroup-statbank',
           'psycopg2',
           'blinker',
           'sqlalchemy',
